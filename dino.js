7cfdcf472d91ecf6a2f682de6f047f1c9ffd927f
DevSillyr <123992449+DevSillyr@users.noreply.github.com>
2024-03-16 23:20:38 +0100

    Add files via upload

---
+++ dino.js
@@ -0,0 +1,166 @@
+//board
+let board;
+let boardWidth = 750;
+let boardHeight = 250;
+let context;
+
+//dino
+let dinoWidth = 80;
+let dinoHeight = 94;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth,
+    height : dinoHeight,
+}
+
+//cactus
+let cactusArray = [];
+
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width  = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+//physics
+let velocityX = -8; //cactus moving left speed
+let velocityY = 0;
+let gravity = 0.4;
+
+let gameOver = false
+let score = 0;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d"); //drawing on the board
+
+    //draw dinosaur
+    //context.fillStyle="green";
+    //context.fillRect(dino.x, dino.y, dino.width, dino.height);
+
+    dinoImg = new Image();
+    dinoImg.src = "./img/dino.png";
+    dinoImg.onload = function() {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
+
+    cactus1Img = new Image()
+    cactus1Img.src = "./img/cactus1.png"
+
+    cactus2Img = new Image()
+    cactus2Img.src = "./img/cactus2.png"
+
+    cactus3Img = new Image()
+    cactus3Img.src = "./img/cactus3.png"
+
+    requestAnimationFrame(update);
+    setInterval(placeCactus, 1000); //1000 ms (1 second)
+    document.addEventListener("keydown", moveDino); //ON BUTTON PRESS
+}
+
+function update() {
+    if(gameOver) {
+        return;
+    }
+
+    requestAnimationFrame(update);
+
+    context.clearRect(0,0,board.width,board.height)
+
+    //dino
+    velocityY += gravity;
+    dino.y = Math.min(dino.y + velocityY, dinoY);
+
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+    //cactus
+    for (let i = 0; i < cactusArray.length; i++) {
+        let cactus = cactusArray[i];
+        cactus.x += velocityX
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+
+        if(detectCollision(dino, cactus)) {
+            gameOver = true;
+            dinoImg.src = "./img/dino-dead.png";
+            dinoImg.onload = function() {
+                context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+            }
+        }
+    }
+
+    //score
+    context.fillStyle="black";
+    context.font = "20px courier";
+    score++;
+    context.fillText("Deine Punkte amk: "+score, 5, 20);
+}
+
+function moveDino(e) {
+    if(gameOver) {
+        return;
+    }
+
+    if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
+        //jump
+        velocityY = -10
+    }
+}
+
+function placeCactus() {
+    if(gameOver) {
+        return;
+    }
+
+    //place cactus
+    let cactus = {
+        img : null,
+        x : cactusX,
+        y : cactusY,
+        width : null,
+        height : cactusHeight,
+    }
+
+    let placeCactusChance = Math.random()
+
+    if (placeCactusChance > .90) { //10% of getting cactus 3
+        cactus.img = cactus3Img;
+        cactus.width = cactus3Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .70) { //30% of getting cactus 2
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > 0.50) { //if not higher than 0.7 then cactus1
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+    }
+
+    if (cactusArray.length > 5) {
+        cactusArray.shift(); //remove the first member of the array (no lag)
+    }
+}
+
+function detectCollision(a, b) {
+    return  a.x < b.x + b.width &&  //a's top left corner
+            a.x + a.width > b.x && //a's top right corner
+            a.y < b.y + b.height && //a's bottom left corner
+            a.y + a.height > b.y //a's bottom right corner (opposite from b (if a top left then b bottom right for example))
+
+}
